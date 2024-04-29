0d648635dd4da782dabbb9a4f050ba964a1f8f4d
unknown <devgdevelopers10@gmail.com>
2024-04-29 19:38:22 +0530

    git about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      About Page
+      This is About Us Page 
     </div>
   )
 }
