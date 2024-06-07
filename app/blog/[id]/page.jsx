10e8558760a6b5d020c4ace3ba665fade997cb44
Abhi8099 <abhishek.sharma1@digranknow.com>
2024-06-07 14:56:16 +0530

    added blog inner pages

---
+++ app/blog/[id]/page.jsx
@@ -0,0 +1,51 @@
+"use client"
+
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation'; 
+
+const BlogPost = () => {
+    const router = useRouter();
+    const [blog, setBlog] = useState("");
+
+    useEffect(() => {
+        const fetchBlog = async () => {
+            try {
+                const id = window.location.pathname.split('/').pop();
+                console.log(id); // Make sure id is correctly extracted
+                if (id) {
+                    const response = await fetch(`/api/blogs?id=${id}`);
+                    const data = await response.json();
+                    console.log(data);
+                    setBlog(data);
+                }
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
+    
+        fetchBlog(); 
+    }, []); 
+
+
+    return (
+<div className="flex min-h-auto flex-col  justify-start md:mt-[8rem]">
+    <div className="md:px-48 md:py-10 py-4 px-4 flex flex-col md:flex-row ">
+
+        {/* Title and Content */}
+        <div className="md:w-3/4 md:pr-4 flex flex-col justify-start gap-4 ">
+            <h2 className="text-2xl font-semibold text-[#2e2e84] ">{blog.title}</h2>
+            <p className="leading-relaxed text-gray-700">{blog.content}</p>
+        </div>
+
+        {/* Image */}
+        <div className="md:w-1/4 md:pl-4">
+            <img src={blog.img} alt={blog.title} height={300} width={300} />
+        </div>
+
+    </div>
+</div>
+
+    );
+};
+
+export default BlogPost;
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,70 +1,72 @@
-"use client"
 
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 
 export default function Page() {
-  
-const [blogs, setBlogs] = useState([]);
-useEffect(() => {
-  fetchBlogs();
-}, []);
 
-const fetchBlogs = () => {
-  fetch('/api/blogs')
-    .then(response => response.json())
-    .then(data => setBlogs(data.data))
-    .catch(error => console.error('Fetch error:', error));
-};
+  useEffect(() => {
+    Aos.init({
+    });
+  }, []);
 
+  const [blogs, setBlogs] = useState([]);
 
-return (
-<div className="flex min-h-screen flex-col items-center justify-start md:mt-[8rem]">
-    <h2 className="text-3xl text-purple font-semibold">Blog</h2>
-    
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
 
-    <div className="p-8 flex justify-center items-center ">
-      <div className="flex flex-wrap m-4  ">
-        {blogs.map(blog => (
-          <div key={blog._id} className="p-4 ">
-            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden shadow-md">
-              <div className="p-6 flex flex-col gap-2 ">
-              <img src={blog.img } alt="" height={300} width={300} />
-                <h2 className="text-xl font-semibold ">{blog.title}</h2>
-                <p className="leading-relaxed text-gray-700">{blog.shortDesc}</p>
-                <Link
-                      href={`/blog/${blog._id}`}
-                      className="inline-flex items-center rounded-md bg-[#52c42f1f] px-4 py-2  text-sm font-medium text-[#2e2e84] transition-colors duration-300 hover:bg-[#52c42f33]"
-                    >
+  const fetchBlogs = async () => {
+    try {
+      const response = await fetch('/api/blogs');
+      const data = await response.json();
+      setBlogs(data.data);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-start md:mt-[8rem]">
+      <h2 className="text-3xl text-purple font-semibold">Blog</h2>
+       <div className="p-8 flex justify-center items-center" data-aos="fade-up" data-aos-duration="700">
+        <div className="flex flex-wrap m-4 ">
+          {blogs.map(blog => (
+            <div key={blog._id} className="p-4">
+              <div className="h-full border-2  border-opacity-60 rounded-lg overflow-hidden shadow-md hover:scale-95 hover:border-  transition-all ease-in-out duration-300">
+                <div className="p-6 flex flex-col gap-2  ">
+                  <img src={blog.img} alt="" height={300} width={300} className=" transition-all ease-in-out duration-300"/>
+                  <h2 className="text-2xl font-semibold text-[#2e2e84]">{blog.title}</h2>
+                  <p className="leading-relaxed text-gray-700">{blog.shortDesc}</p>
+                  <Link href={`/blog/${blog._id}`} className=" w-[19rem] inline-flex  items-center rounded-md bg-[#52c42f1f] px-4 py-2 text-sm font-medium text-[#2e2e84] transition-colors duration-300 hover:bg-[#52c42f33]">
                       Read More
-                    </Link>
-
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
+          ))}
+        </div>
+      </div> 
 
 
 
 
-
-    <section className="">
+      <section className="">
         <div className="container mx-auto flex flex-col md:flex-row p-3">
           <div className="w-[100%] lg:w-[50%] my-auto mx-auto order-2 lg:order-1">
             <h2 className="text-xl font-semibold text-purple lg:text-left text-center">
               Stay Informed with Our Newsletter
             </h2>
-            <p className=" font-normal text-green my-4 lg:text-left text-justify">
+            <p className="font-normal text-green my-4 lg:text-left text-justify">
               Subscribe to our newsletter and be the first to hear about our
               latest innovations, industry insights, and sustainability tips.
               Join our community and stay connected with Organicco's mission to
               create a greener future.
             </p>
-
             <form action="" className="flex my-3 gap-3">
               <input
                 type="email"
@@ -87,10 +89,10 @@ return (
               height={100}
               alt="newsletter"
               unoptimized
-            ></Image>
+            />
           </div>
         </div>
       </section>
-</div>
-)
+    </div>
+  );
 }
